57e0774b72220de084e1ce6afddec53d108fef02
Gabchak <andrey.gabchak@gmail.com>
2019-06-28 19:22:44 +0300

    sum field is added to record table

--- sql/src/main/resources/db.migration/V1.0.0__initial_data.sql
+++ sql/src/main/resources/db.migration/V1.0.0__initial_data.sql
@@ -10,7 +10,7 @@ CREATE TABLE wallet
 (
     id                          SERIAL       NOT NULL PRIMARY KEY,
     name                        VARCHAR(100) NOT NULL,
-    balance                     NUMERIC(2)          NOT NULL,
+    balance                     NUMERIC(2)   NOT NULL,
     is_income                   BOOLEAN      NOT NULL,
     is_count_in_general_balance BOOLEAN      NOT NULL,
     fk_currency_id              INT,
@@ -27,11 +27,12 @@ CREATE TABLE category
 
 CREATE TABLE record
 (
-    id             SERIAL  NOT NULL PRIMARY KEY,
-    fk_wallet_id   INT     NOT NULL,
-    fk_category_id INT     NOT NULL,
-    is_income      BOOLEAN NOT NULL,
-    date           DATE    NOT NULL,
+    id             SERIAL     NOT NULL PRIMARY KEY,
+    sum            NUMERIC(2) NOT NULL,
+    fk_wallet_id   INT        NOT NULL,
+    fk_category_id INT        NOT NULL,
+    is_income      BOOLEAN    NOT NULL,
+    date           DATE       NOT NULL,
     comment        VARCHAR(255),
     FOREIGN KEY (fk_wallet_id) REFERENCES wallet (id),
     FOREIGN KEY (fk_category_id) REFERENCES category (id)
