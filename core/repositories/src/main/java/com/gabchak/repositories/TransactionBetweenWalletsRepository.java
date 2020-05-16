291ab14b9efc4d005d36e8743684c2f1e9401479
Gabchak <andrey.gabchak@gmail.com>
2020-05-16 23:10:55 +0300

    new gradle build

--- core/repositories/src/main/java/com/gabchak/repositories/TransactionBetweenWalletsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gabchak.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionBetweenWalletsRepository extends JpaRepository<TransactionBetweenWalletsRepository, Integer> {
-}
