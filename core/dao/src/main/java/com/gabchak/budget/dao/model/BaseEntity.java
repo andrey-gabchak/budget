b141b51b839efa2d26ac3c096802b1f1041b6cae
Gabchak <andrey.gabchak@gmail.com>
2020-05-22 11:21:09 +0300

    make BaseEntity class public

--- core/dao/src/main/java/com/gabchak/budget/dao/model/BaseEntity.java
+++ core/dao/src/main/java/com/gabchak/budget/dao/model/BaseEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.MappedSuperclass;
 @Getter
 @Setter
 @MappedSuperclass
-abstract class BaseEntity {
+public abstract class BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
