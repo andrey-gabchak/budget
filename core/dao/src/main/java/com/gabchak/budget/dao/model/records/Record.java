c4f22149651ef9b3d3bae10d808067a66b215adc
Gabchak <andrey.gabchak@gmail.com>
2020-06-03 23:15:13 +0300

    add relation to currency

--- core/dao/src/main/java/com/gabchak/budget/dao/model/records/Record.java
+++ core/dao/src/main/java/com/gabchak/budget/dao/model/records/Record.java
@@ -1,13 +1,18 @@
 package com.gabchak.budget.dao.model.records;
 
+import com.gabchak.budget.dao.model.Currency;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.MappedSuperclass;
 
 @Getter
@@ -25,4 +30,10 @@ abstract class Record {
   private String description;
   @Column(name = "CREATION_DATE")
   private LocalDateTime creationDate;
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "RECORD_CURRENCY",
+      joinColumns = {@JoinColumn(name = "RECORD_ID")},
+      inverseJoinColumns = {@JoinColumn(name = "CURRENCY_ID")}
+  )
+  private Currency currency;
 }
